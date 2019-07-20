7e4c92974215807ed14adb55630943a04b0782d4
abhaya3108 <48272105+abhaya3108@users.noreply.github.com>
2019-07-20 12:03:53 +0530

    Fibonacci and Palindrome
    Fibonacci and Palindrome

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 18:25:38 2019
+
+@author: panda4
+"""
+
+import fibo
+
+fibo.fib2(1000)
---
+++ palindromeexec.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 18:41:10 2019
+
+@author: panda4
+"""
+
+import palindrome
+
+palindrome.ispalindrome("dad")
